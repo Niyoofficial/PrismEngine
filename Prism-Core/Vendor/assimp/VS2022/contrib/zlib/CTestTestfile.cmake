8cea18d590d55b4cba5cbdaea00dbfb3e706f02a
Niyo <dawidm200301@gmail.com>
2024-06-22 14:53:32 +0200

    Missing dependencies

---
+++ Prism-Core/Vendor/assimp/VS2022/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: F:/VisualStudio/PrismEngine/Prism-Core/Vendor/assimp/contrib/zlib
+# Build directory: F:/VisualStudio/PrismEngine/Prism-Core/Vendor/assimp/VS2022/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Prism-Core/Vendor/xxHash_cpp/Build-xxHash_cpp.lua
@@ -0,0 +1,4 @@
+project "xxHash_cpp"
+	kind "None"
+
+	files { "include/xxhash.hpp", "xxHash.h" }
