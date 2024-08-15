bcaafd2acb82c03eabeab04077f278bf2f8cd893
Dawid Markiewicz <dawidm200301@gmail.com>
2024-08-15 14:55:35 +0100

    Removed accidentaly added generated files

--- Prism-Core/Vendor/assimp/VS2022/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/VisualStudio/PrismEngine/Prism-Core/Vendor/assimp/contrib/zlib
-# Build directory: F:/VisualStudio/PrismEngine/Prism-Core/Vendor/assimp/VS2022/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
